69652d7c9df4378f8c9ec1bfbc6374da683826e9
PeterWriteCode <pedrombrancomendes@gmail.com>
2023-11-23 21:25:29 +0000

    Atualização

--- src/Art.js
+++ src/Art.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainItemsContainer from "./MainItemsContainer";
 import jquery from "jquery";
+import art from "./media/art.png";
+import name from "./media/name.png";
+import mini from "./media/mini.png";
+import comms from "./media/comms.png";
+import about from "./media/about.png";
+import contacts from "./media/contacts.png";
+import { withRouter } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./Art.css";
 
 class Art extends React.Component {
+  
   constructor(props) {
     super(props);
 
@@ -15,6 +24,13 @@ class Art extends React.Component {
     };
   }
 
+  componentDidMount() {
+    // ... existing code
+
+    // The navigate function is now available through this.props
+    
+  }
+  
   componentDidMount() {
     // IE, Chrome, Safari, Opera
     window.addEventListener("wheel", this.scrollHandler);
@@ -32,9 +48,12 @@ class Art extends React.Component {
       "transform",
       `translateY(-${jquery(".container-content").height()}px)`
     );
+
+    this.navigate = this.props.history.push;
   }
 
   scrollHandler = event => {
+    
     this.setState(state => {
       let newDeltaY = state.deltaY + event.deltaY;
 
@@ -45,13 +64,39 @@ class Art extends React.Component {
         deltaY: newDeltaY
       };
     });
+   
   };
+ 
+  
 
   render() {
+    
+    
+    
+
     return (
+      
       <div className="App">
         <MainItemsContainer {...this.state} />
+        <div className="Container">
+          <div className="art" onClick={() => this.navigate("/art")}>
+            <img alt="art" className="art" draggable="false" src={art}></img>
+          </div>
+          <div className="mini  " onClick={() => this.navigate("/miniatures")}>
+            <img alt="mini" className="mini" draggable="false" src={mini}></img>
+          </div>
+          <div className="comms" onClick={() => this.navigate("/commissions")}>
+            <img alt="comms" className="comms" draggable="false" src={comms}></img>
+          </div>
+          <div className="about" onClick={() => this.navigate("/about")}>
+            <img alt="about" className="about" draggable="false" src={about}></img>
+          </div>
+          <div className="contacts" onClick={() => this.navigate("/contacts")}>
+            <img alt="contacts" className="contacts" draggable="false" src={contacts}></img>
+          </div>
       </div>
+      </div>
+      
     );
   }
 }
@@ -59,4 +104,4 @@ class Art extends React.Component {
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Art />, rootElement);
 
-export default Art;
+export default withRouter(Art);
--- src/MainItemsContainer.js
+++ src/MainItemsContainer.js
@@ -1,4 +1,10 @@
 import React from "react";
+import capa from "./media/comic/Capa.jpg"
+import cabra40 from"./media/comic/CAPA TOP ZIXO.png";
+import tacho from "./media/comic/cartoon.jpg";
+import smtuc from "./media/comic/smtuc.png";
+import twd from "./media/comic/1690906335597.png";
+import spider from "./media/comic/1690906012585.png";
 
 class MainItemsContainer extends React.Component {
   render() {
@@ -19,21 +25,57 @@ class MainItemsContainer extends React.Component {
     return (
       <div className="main_items_container">
         <section className="main_items_container_child">
-          <div className="container-content left" style={style_first}>
-            {numbers.map(i => (
-              <div key={i} className="container-content-block">
-                LEFT {i}
-              </div>
-            ))}
+          <div className="container-content left"  style={style_first}>
+          <div className="container-content-block">
+              <img className="smtuc" draggable="false"src={smtuc}></img>
+            </div>
+          <div className="container-content-block">
+              <img className="spider" draggable="false"src={spider}></img>
+            </div>
+          <div className="container-content-block">
+              <img className="twd" draggable="false"src={twd}></img>
+            </div>
+          
+            <div className="container-content-block">
+              <img className="tacho" draggable="false"src={tacho}></img>
+            </div>
+            <div className="container-content-block">
+              <img className="cabra40"draggable="false" src={cabra40}></img>
+            </div>
+            <div className="container-content-block">
+              <img className="comic"draggable="false" src={capa}></img>
+            </div>
+            
+              
           </div>
         </section>
         <section className="main_items_container_child">
           <div className="container-content right" style={style_second}>
-            {numbers.map(i => (
-              <div key={i} className="container-content-block">
-                RIGHT {i}
-              </div>
-            ))}
+           
+            
+            <div className="container-content-block">
+            <text><strong><span style={{color: "#ed2124"}}>MERCY</span></strong>, written and illustrated by Pedro Branco is a horror/thriller short comic taking place a year (or so thinks the main character) after a zombie outbreak. 
+             <br></br>Join Jo and Arthur as they manage their way through a small issue: One of them is sick, and the only place where they can find medicine has rumours of being perilous.
+            </text>
+             </div>
+            <div className="container-content-block">
+              <text>Cover made for <strong>Jornal Universitário A Cabra's 40th Anniversary edition.</strong> "Cabra" in portuguese refers to the female goat, so the badass-looking character in the cover, is wearing the female version of the university uniform. The cake pops with the colors of the event, while the rest is in black and white to contrast. <br></br>The background features old covers of the newspaper.</text>
+            </div>
+            <div className="container-content-block">
+              <text>Cartoon criticizing <strong>Associação Académcia de Coimbra</strong>, commenting on their greediness regarding the prices of the student fests.</text>
+            </div>
+            <div className="container-content-block">
+              <text>Fan-Art illustration of Rick and Carl Grimes from <strong>The Walking Dead</strong></text>
+            </div>
+            <div className="container-content-block">
+              <text>
+                  Fan-Art illustration of <strong>Spider-Man</strong>.
+              </text>
+            </div>
+            <div className="container-content-block">
+              <text>Cartoon criticizing the poor conditions of the old and overworn buses used by <strong>Serviços Municipalizados de Transportes Urbanos de Coimbra </strong>(SMTUC)</text>
+            </div>
+            
           </div>
         </section>
       </div>
